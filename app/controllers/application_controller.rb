43c89b462fe3e4e2c36af06c477018aef6df8332
Danil Nurgaliev <jkonalegi@gmail.com>
2014-02-19 13:57:02 +0400

    auth-token bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name,:sex,:surname,:birthdate,:email,:password,:password_confirmation)}
   end
 
+  after_filter :set_csrf_cookie_for_ng
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
+  
+
 end
